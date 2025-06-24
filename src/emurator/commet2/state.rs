c51d4824bb121d57489ee58246752869cdb31368
371tti <109227273+371tti@users.noreply.github.com>
2025-06-24 23:06:45 +0900

    CPUState構造体のフィールド名を小文字に変更し、GeneralRegister構造体の名前を修正しました。

--- src/emurator/commet2/state.rs
+++ src/emurator/commet2/state.rs
@@ -2,33 +2,33 @@ pub struct CPUState {
     /// マシンサイクルのカウンタ
     pub cycle: u8,
     /// 汎用レジスタ
-    pub GR: GeneralResister,
-    /// プログラムレジスタ 
-    pub PR: u16,
+    pub gr: GeneralRegister,
+    /// プログラムレジスタ
+    pub pr: u16,
     /// メモリアドレスレジスタ
-    pub MAR: u16,
+    pub mar: u16,
     /// メモリリードレジスタ
-    pub MRR: u16,
+    pub mrr: u16,
     /// スタックポインタ
-    pub SP: u16,
+    pub sp: u16,
     /// メモリ
     pub memory: Memory,
     /// 命令レジスタ
-    pub IR: [u16; 2],
+    pub ir: [u16; 2],
     /// フラグレジスタ
-    pub FR: [bool; 3],
+    pub fr: [bool; 3],
 }
 
 /// 汎用レジスタの構造体
-pub struct GeneralResister {
-    pub GR0: u16,
-    pub GR1: u16,
-    pub GR2: u16,
-    pub GR3: u16,
-    pub GR4: u16,
-    pub GR5: u16,
-    pub GR6: u16,
-    pub GR7: u16,
+pub struct GeneralRegister {
+    pub gr0: u16,
+    pub gr1: u16,
+    pub gr2: u16,
+    pub gr3: u16,
+    pub gr4: u16,
+    pub gr5: u16,
+    pub gr6: u16,
+    pub gr7: u16,
 }
 
 /// メインメモリ
