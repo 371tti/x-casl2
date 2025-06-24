ddff5df7beb0ae3536125bedf5d23c6875fdb436
371tti <sannnanaitti@gmail.com>
2025-06-24 12:58:40 +0900

    ALUトレイトの実装を追加し、CPU構造体に必要な演算関数を定義しました。

--- src/emurator/commet2/alu.rs
+++ src/emurator/commet2/alu.rs
@@ -16,6 +16,56 @@ trait ALU {
     fn cpl(&mut self, a: u16, b: u16) -> bool;
 }
 
-impl CPU {
-    
+impl ALU for CPU {
+    fn and(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn or(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn xor(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn adda(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn suba(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn addl(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn subl(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn sla(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn sra(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn sll(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn srl(&mut self, a: u16, b: u16) -> u16 {
+        todo!()
+    }
+
+    fn cpa(&mut self, a: u16, b: u16) -> bool {
+        todo!()
+    }
+
+    fn cpl(&mut self, a: u16, b: u16) -> bool {
+        todo!()
+    }
 }
